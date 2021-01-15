c55474c4bc16bfbca3737e715913c3e88057ef8e
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2021-01-15 12:41:36 -0300

    Starting a DB schema

---
+++ backend/migrations/create_schema.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect('users.db')
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE users (
+        UserID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL,
+        senha TEXT NOT NULL
+    );
+""")
+
+cursor.execute("""
+    CREATE TABLE receitas (
+        ReceitaID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT NOT NULL,
+        texto TEXT NOT NULL,
+        autor INTEGER NOT NULL,
+        FOREIGN KEY (autor) REFERENCES users(UserID)
+    );
+""")
+
+
+
+print('Users table created')
+connection.close()
