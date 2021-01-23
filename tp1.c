51e81822f0b86262c32b9eb9f8a8bba5140ba962
joaomcouto <joaom.couto55@gmail.com>
2021-01-23 14:48:20 -0300

    Logic fully functional if fed random Ids, pending proper id selection per business logic ; pending raj thread interferances based of on returned id code

--- tp1.c
+++ tp1.c
@@ -11,22 +11,33 @@ struct persona
 };
 
 int nAttempts;
-int idNext ;
+int idNext = -1;
 #define personaCount 8
 
-int ovenInterest[personaCount] ;
+int ovenInterest[personaCount];
 
-int interestCount = 0 ;
+int interestCount = 0;
 
-pthread_mutex_t interestMutex ;
-pthread_mutex_t ovenMutex ;
+pthread_mutex_t interestMutex;
+pthread_mutex_t ovenMutex;
+pthread_mutex_t initMutex;
 
 //pthread_cond_t coupleGate ;
-//pthread_cond_t 
+//pthread_cond_t
 
-pthread_cond_t ovenGate[personaCount] ;
-pthread_cond_t updateGate ;
+pthread_cond_t ovenGate[personaCount];
+pthread_cond_t updateGate;
 
+int randomInt(int a, int b)
+{
+    /*
+    Retorna um inteiro aleatório no intervalo [a, b]
+    */
+    int rand = (int)1000 * drand48();
+    return (rand % (b - a)) + a;
+}
+
+int init = 1;
 
 /*
 int fetch_next(){
@@ -56,52 +67,134 @@ int fetch_next(){
 
 /////////////
 //GLOBAL VAR: int init = 1 ;
-wait_oven(struct persona * p){
+void wait_oven(struct persona *p)
+{
+    sleep(randomInt(3, 6));
     pthread_mutex_lock(&initMutex);
-    if(init == 1) {
-        idNext = p->id ; 
-        init++ ;
-    }
-    else{
+    if (init == 1)
+    {
+        idNext = p->id;
+        init++;
+
         pthread_mutex_lock(&interestMutex);
-        ovenInterest[p->id] = 1 ;
+        ovenInterest[p->id] = 1;
         printf("%s quer usar o forno\n", p->name);
+
+        //deee
+        printf("[");
+        for (int i = 0; i < personaCount; i++)
+        {
+            printf("%d", ovenInterest[i]);
+        }
+        printf("]\n");
+        //buugggg
+
         pthread_mutex_unlock(&interestMutex);
-    } 
+    }
+    else
+    {
+        pthread_mutex_lock(&interestMutex);
+        ovenInterest[p->id] = 1;
+        printf("%s quer usar o forno\n", p->name);
+
+        //deee
+        printf("[");
+        for (int i = 0; i < personaCount; i++)
+        {
+            printf("%d", ovenInterest[i]);
+        }
+        printf("]\n");
+        //buugggg
+
+        pthread_mutex_unlock(&interestMutex);
+    }
     pthread_mutex_unlock(&initMutex);
 
     pthread_mutex_lock(&ovenMutex);
 
-    while(idNext != p->id){
-        thread_cond_wait(&ovenGate[p->id], &ovenMutex);
+    while (idNext != p->id)
+    {
+        pthread_cond_wait(&ovenGate[p->id], &ovenMutex);
     }
     //Nesse ponto o use_oven abaixo é chamado, ele tem que fazer o print de usando e quando sair pedir pra atualizar o nextId
 }
 
-use_oven(struct persona * p){
+int getIdNext()
+{
+    int amountWaiting = 0;
+    for (int i = 0 ; i < personaCount ; i++){
+        if (ovenInterest[i] == 1) amountWaiting = amountWaiting + 1 ;
+    }
+    if(amountWaiting!=0){
+        int * waitingIndexes = (int *) malloc(sizeof(int) * amountWaiting) ;
+        
+        int j = 0 ;
+        for (int i =0  ; i < personaCount ; i++){
+            if (ovenInterest[i] == 1) {
+                waitingIndexes[j] = i ;
+                j = j + 1 ;
+            }
+        }
+
+        int rando = randomInt(0, amountWaiting );
+        //printf("Get index pegou o random %d dentre %d e ta mandando o index %d\n", rando,amountWaiting, waitingIndexes[rando]) ;
+        int a = waitingIndexes[rando] ;
+        free(waitingIndexes);
+        return a ;
+    } else {
+        init = 1 ;
+        return -1 ;
+    }
+
+    //int rando = randomInt(0, 8);
+    //while (ovenInterest[rando] != 1)
+      //  rando = randomInt(0, 8);
+        //printf("%d", rando) ;
+
+    //return rando;
+}
+
+void use_oven(struct persona *p)
+{
     printf("%s começar a esquenta algo\n", p->name);
-    sleep(1) ;
+    sleep(1);
 
     pthread_mutex_lock(&interestMutex); //Impede entrada de novos durante o calculo do proximo em cima do vetor ovenInterest
 
-    idNext = getIdNext() ; //Dentro dessa função tem que ter a porra toda de calcular quem é o proximo
-    pthread_mutex_unlock(&ovenMutex);
-
-    pthread_cond_signal(&ovenGate[idNext]); //Experimentar com a possibilidade de fazer com apenas um ovenGate e n um array
-
-    pthread_mutex_unlock(&interestMutex); 
+    ovenInterest[p->id] = 0;
+    idNext = getIdNext(); //Dentro dessa função tem que ter a porra toda de calcular quem é o proximo
     
+
+    //ovenInterest[p->id] = 0;
+    printf("%s vai comer\n", p->name);
+
+    //deee
+    
+
+    printf("[");
+    for (int i = 0; i < personaCount; i++)
+    {
+        printf("%d", ovenInterest[i]);
+    }
+    printf("]\n");
+    
+    //printf("O escolhido foi %d\n", idNext);
+    //buugggg
+    pthread_mutex_unlock(&ovenMutex);
+    pthread_cond_signal(&ovenGate[idNext]); //Experimentar com a possibilidade de fazer com apenas um ovenGate e n um array
+    pthread_mutex_unlock(&interestMutex);
+
+    
+
+
 
     //thread_cond_signal(&updateGate) ;
 }
 
-
-
-
 //void * oven(void * data){
-  //  pthread_mutex_lock(&interestMutex);
+//  pthread_mutex_lock(&interestMutex);
 
-   // thread_cond_wait(&ovenGate[i], &ovenMutex);
+// thread_cond_wait(&ovenGate[i], &ovenMutex);
 //}
 
 /////////
@@ -166,32 +259,39 @@ use_oven(struct persona * p){
 }
 */
 
-eat(struct persona * p){
-
-} 
-
-work(struct persona * p){
-
-} 
-
-void * oven_user(void * data)
+void eat(struct persona *p)
 {
-    struct persona * p = (struct persona *) data ;
+    sleep(randomInt(3, 6));
+}
+
+void work(struct persona *p)
+{
+    printf("%s voltou para o trabalho\n", p->name);
+    sleep(randomInt(3, 6));
+}
+
+void *oven_user(void *data)
+{
+    struct persona *p = (struct persona *)data;
     int i;
-    for(i = 0 ; i < nAttempts ; i++){
-        wait_oven(p) ;
-        use_oven(p) ;
-        eat(p) ;
-        work(p) ;
-    } 
-    pthread_exit(EXIT_SUCCESS) ;
+    for (i = 0; i < nAttempts; i++)
+    {
+        wait_oven(p);
+        use_oven(p);
+        eat(p);
+        work(p);
+    }
+    pthread_exit(EXIT_SUCCESS);
 }
 
 int main(int agrc, char **argv)
 {
-
+    //printf("Cheguei aqui\n");
     pthread_mutex_init(&interestMutex, NULL);
+    pthread_mutex_init(&ovenMutex, NULL);
+    pthread_mutex_init(&initMutex, NULL);
 
+    for (int i = 0; i < personaCount; i++) pthread_cond_init(&ovenGate[i], NULL) ;
 
     nAttempts = atoi(argv[1]);
     char *names[personaCount] = {
@@ -202,8 +302,7 @@ int main(int agrc, char **argv)
         "Leonard",
         "Penny",
         "Kripke",
-        "Stuart"
-        };
+        "Stuart"};
 
     struct persona **personaArray = (struct persona **)malloc(personaCount * sizeof(struct persona));
 
@@ -213,27 +312,27 @@ int main(int agrc, char **argv)
         personaArray[i] = (struct persona *)malloc(sizeof(struct persona));
         personaArray[i]->id = i;
         personaArray[i]->name = names[i];
-        ovenInterest[i] = 0 ;
+        ovenInterest[i] = 0;
     }
 
-    pthread_t ovenTid ;
-    pthread_create(&ovenTid, NULL, oven , personaArray[i]) ;
+    // pthread_t ovenTid ;
+    //pthread_create(&ovenTid, NULL, oven , personaArray[i]) ;
 
     pthread_t personaTids[personaCount];
     for (i = 0; i < personaCount; i++)
     {
-        pthread_create(&personaTids[i], NULL, oven_user, personaArray[i])
+        pthread_create(&personaTids[i], NULL, oven_user, personaArray[i]);
     }
 
-    pthread_t rajTid;
-    pthread_create(&rajTid, NULL, oven_checker, NULL);
+    // pthread_t rajTid;
+    //pthread_create(&rajTid, NULL, oven_checker, NULL);
 
-    for (i = 0; i < personaCount; i++){
-        pthread_join(personaTids[i], NULL) ;
-    }    
+    for (i = 0; i < personaCount; i++)
+    {
+        pthread_join(personaTids[i], NULL);
+    }
 
-
-    pthread_cancel(rajTid) ;
+    //pthread_cancel(rajTid) ;
 
     return 0;
 }
