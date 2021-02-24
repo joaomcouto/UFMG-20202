831cddbe2f7515378b765f5462c4cf0684dbebe5
joaomcouto <joaom.couto55@gmail.com>
2021-02-24 11:39:01 -0300

    Seperated profile route lists

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import CardList from '../UX/CardList/CardList';
 import Button from 'react-bootstrap/Button';
@@ -6,13 +7,15 @@ import classes from './Profile.module.css';
 import recipesMock from '../../recipesMock.json';
 
 const Profile = () =>{
-    const [recipes, setRecipes] = React.useState([]);
+    const [favoriteRecipes, setFavoriteRecipes] = React.useState([]);
+    const [userRecipes, setUserRecipes] = React.useState([]);
 
     React.useEffect(() => {
         if(process.env.REACT_APP_IS_SERVER_WORKING !== 'false'){
           // Fetch API
         } else {
-          setRecipes(recipesMock.data.recipes.slice(0, 5))
+          setUserRecipes(recipesMock.data.recipes.slice(0, 5))
+          setFavoriteRecipes(recipesMock.data.recipes.slice(0, 5))
         }
       }, []);
 
@@ -21,7 +24,7 @@ const Profile = () =>{
     
           <div >
             <h4 className={classes.header}>Minhas Receitas:</h4>
-            <CardList className={`${classes.List}`} list={recipes}/>
+            <CardList className={`${classes.List}`} list={userRecipes}/>
             <div className={['w-100', 'text-right'].join` `}>
               <Button variant="link" href="/recipes">Ver mais...</Button>
             </div>
@@ -29,7 +32,7 @@ const Profile = () =>{
           
           <div>
           <h4 className={classes.header}>Receitas favoritas:</h4>
-          <CardList list={recipes}/>
+          <CardList list={favoriteRecipes}/>
           <div className={['w-100', 'text-right'].join` `}>
             <Button variant="link" href="/recipes">Ver mais...</Button>
           </div>
